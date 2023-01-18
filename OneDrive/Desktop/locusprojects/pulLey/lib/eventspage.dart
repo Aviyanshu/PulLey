12776b88822e376356cd6d94643f64aa78fb824e
Aditi Kharel <077bei008.aditi@pcampus.edu.np>
2023-01-18 23:23:19 +0545

    pages and nav bar added

---
+++ OneDrive/Desktop/locusprojects/pulLey/lib/eventspage.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
+
+import 'package:flutter/material.dart';
+
+class EventsPage extends StatelessWidget {
+  const EventsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Events Page"),
+    );
+  }
+}

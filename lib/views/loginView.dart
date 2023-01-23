597053b9dcccfc26facc519e1b396215d0113224
Aditi Kharel <077bei008.aditi@pcampus.edu.np>
2023-01-23 10:40:32 +0545

    login ui made

--- lib/views/loginView.dart
+++ lib/views/loginView.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:pulley/Colors.dart';
 
 import 'dart:developer' as devtools show log;
 
@@ -36,115 +37,115 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("PulLey-Login"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(
-            controller: _email,
-            enableSuggestions: false,
-            autocorrect: false,
-            keyboardType: TextInputType.emailAddress,
-            decoration: const InputDecoration(
-              hintText: 'Enter your email here',
-              border: OutlineInputBorder(),
-            ),
-          ),
-          TextField(
-            controller: _password,
-            obscureText: true,
-            enableSuggestions: false,
-            autocorrect: false,
-            decoration: const InputDecoration(
-              hintText: 'Enter your password here',
-              border: OutlineInputBorder(),
-            ),
-          ),
-          TextButton(
-            onPressed: () async {
-              final email = _email.text;
-              final password = _password.text;
+        
+      body:Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('lib/assets/login.png'),fit: BoxFit.cover )
+        ),
+        child: Container(
+          padding: EdgeInsets.only(top:MediaQuery.of(context).size.height*0.25,right:35,left:35),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+                  TextField(
+                  controller: _email,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration:  InputDecoration(
+                    hintText: 'Enter your email here',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+              const SizedBox(height: 15,),
+              TextField(
+                controller: _password,
+                obscureText: true,
+                enableSuggestions: false,
+                autocorrect: false,
+                decoration:  InputDecoration(
+                  hintText: 'Enter your password here',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20)
+                  ),
+                ),
+              ),
+              SizedBox(height: 40,),
+              TextButton(
+                onPressed: () async {
+                  final email = _email.text;
+                  final password = _password.text;
 
-              try {
-                final signedUser =
-                    await FirebaseAuth.instance.signInWithEmailAndPassword(
-                  email: email,
-                  password: password,
-                );
-                SharedPreferences prefs = await SharedPreferences.getInstance();
-                prefs.setBool('isLoggedIn', true);
-                User userLoggedin = FirebaseAuth.instance.currentUser!;
-                final DocumentSnapshot doc_ = await FirebaseFirestore.instance
-                    .collection('users')
-                    .doc(userLoggedin.uid)
-                    .get();
-                devtools.log(userLoggedin.uid);
-                devtools.log(doc_.toString());
-                if (doc_.exists) {
-                  devtools.log(doc_.data.toString());
-                  final data = doc_.data() as Map<String, dynamic>;
-                  final role = data['role'];
-                  devtools.log(role);
-                  if (role == 'Student') {
-                    Navigator.of(context).pushNamedAndRemoveUntil(
-                      studentRoute,
-                      (route) => false,
+                  try {
+                    final signedUser =
+                        await FirebaseAuth.instance.signInWithEmailAndPassword(
+                      email: email,
+                      password: password,
                     );
-                  } else if (role == 'Club') {
-                    Navigator.of(context).pushNamedAndRemoveUntil(
-                      clubRoute,
-                      (route) => false,
-                    );
-                  } else if (role == 'Organisation') {
-                    Navigator.of(context).pushNamedAndRemoveUntil(
-                      organisationRoute,
-                      (route) => false,
-                    );
+                    SharedPreferences prefs = await SharedPreferences.getInstance();
+                    prefs.setBool('isLoggedIn', true);
+                    User userLoggedin = FirebaseAuth.instance.currentUser!;
+                    final DocumentSnapshot doc_ = await FirebaseFirestore.instance
+                        .collection('users')
+                        .doc(userLoggedin.uid)
+                        .get();
+                    devtools.log(userLoggedin.uid);
+                    devtools.log(doc_.toString());
+                    if (doc_.exists) {
+                      devtools.log(doc_.data.toString());
+                      final data = doc_.data() as Map<String, dynamic>;
+                      final role = data['role'];
+                      devtools.log(role);
+                      if (role == 'Student') {
+                        Navigator.of(context).pushNamedAndRemoveUntil(
+                          studentRoute,
+                          (route) => false,
+                        );
+                      } else if (role == 'Club') {
+                        Navigator.of(context).pushNamedAndRemoveUntil(
+                          clubRoute,
+                          (route) => false,
+                        );
+                      } else if (role == 'Organisation') {
+                        Navigator.of(context).pushNamedAndRemoveUntil(
+                          organisationRoute,
+                          (route) => false,
+                        );
+                      }
+                    } else {
+                      devtools.log('Error');
+                    }
+                  } on FirebaseAuthException catch (e) {
+                    if (e.code == 'user-not-found') {
+                      devtools.log("User not found");
+                    } else if (e.code == 'wrong-password') {
+                      devtools.log("Wrong Password");
+                    }
                   }
-                } else {
-                  devtools.log('Error');
-                }
-              } on FirebaseAuthException catch (e) {
-                if (e.code == 'user-not-found') {
-                  devtools.log("User not found");
-                } else if (e.code == 'wrong-password') {
-                  devtools.log("Wrong Password");
-                }
-              }
-            },
-            child: const Text("Sign in"),
-          ),
-          Container(
-            width: 200,
-            height: 50,
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.blueGrey,
-                width: 2,
-              ),
-            ),
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: TextButton(
-                onPressed: () {
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    registerRoute,
-                    (route) => false,
-                  );
                 },
-                child: const Text("Not Registered? Click Here"),
+                child: const Text("Sign in",
+                style: TextStyle(color:lightblueColor),),
               ),
-            ),
+              
+                 Align(
+                  alignment: Alignment.bottomCenter,
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamedAndRemoveUntil(
+                        registerRoute,
+                        (route) => false,
+                      );
+                    },
+                    child: const Text("Not Registered? Click Here",
+                    style: TextStyle(color: lightblueColor) ,),
+                  ),
+                ),
+            ],
           ),
-          TextButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            child: const Text('Sign out'),
-          ),
-        ],
+        ),
       ),
     );
   }
